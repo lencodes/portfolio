d23c4537966d5a49059460e0b2453b8915c9cb00
Lencodes <contact@lencodes.com>
2023-12-15 18:44:41 +0100

    ADD RecentProjectsTemplate

---
+++ src/data/recentProjects.tsx
@@ -0,0 +1,28 @@
+import { TProjectCard } from "../components/projectCard/ProjectCard";
+
+export const recentProjects: TProjectCard[] = [
+  {
+    title: "Lead Frontend Developer",
+    subtitle: "Conduction, Amsterdam",
+    logoSrc: "/images/projects/conduction/logo-conduction.svg",
+    backgroundImageSrc: "/images/projects/conduction/card-image-conduction.png",
+  },
+  {
+    title: "Frontend Developer",
+    subtitle: "Kaliber Interactive, Utrecht",
+    logoSrc: "/images/projects/kaliber/logo-kaliber.svg",
+    backgroundImageSrc: "/images/projects/kaliber/card-image-kaliber.png",
+  },
+  {
+    title: "Lead Frontend Developer",
+    subtitle: "Koopwebsite, Remote",
+    logoSrc: "/images/projects/koopwebsite/logo-koopwebsite.svg",
+    backgroundImageSrc: "/images/projects/koopwebsite/card-image-koopwebsite.png",
+  },
+  {
+    title: "Frontend Developer",
+    subtitle: "Level.works, Rotterdam",
+    logoSrc: "/images/projects/level-works/logo-level-works.svg",
+    backgroundImageSrc: "/images/projects/level-works/card-image-level-works.png",
+  },
+];
