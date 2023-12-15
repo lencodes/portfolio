c75882804a37e4d1a766493e2b5187f500613c04
Lencodes <contact@lencodes.com>
2023-12-15 18:45:03 +0100

    REFACTOR small cleanup thoughout the application

--- src/templates/home/HomeTemplate.tsx
+++ src/templates/home/HomeTemplate.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import styles from "./HomeTemplate.module.scss";
 import { Container } from "../../components/container/Container";
 import { HomeHeaderTemplate } from "../templateParts/homeHeader/HomeHeaderTemplate";
+import { RecentProjectsGridTemplate } from "../templateParts/recentProjectsGrid/RecentProjectsGridTemplate";
 
 export const HomeTemplate: React.FC = () => {
   return (
     <Container layoutClassName={styles.container}>
-      <HomeHeaderTemplate />
+      <section>
+        <HomeHeaderTemplate />
+      </section>
+
+      <section>
+        <RecentProjectsGridTemplate />
+      </section>
     </Container>
   );
 };
