f282eff791c823d289245e47f46cd4077abf592a
Lencodes <contact@lencodes.com>
2023-12-15 12:41:09 +0100

    ADD (init) HomeTemplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./styling/global.module.scss";
 import styles from "./App.module.scss";
+import { HomeTemplate } from "./templates/home/HomeTemplate";
 
 const App: React.FC = () => {
-  return <div className={styles.container}>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <HomeTemplate />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/templates/home/HomeTemplate.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./HomeTemplate.module.scss";
+import { Container } from "../../components/container/Container";
+import LENNART_BANK_PROFILE_PICTURE from "../../resources/global-images/lennart-bank-profile-picture.jpg";
+
+export const HomeTemplate: React.FC = () => {
+  return (
+    <Container layoutClassName={styles.container}>
+      <section className={styles.header}>
+        <div className={styles.headshot}>
+          <img src={LENNART_BANK_PROFILE_PICTURE} alt="Lennart Bank" />
+
+          <h2>
+            Lennart Bank, B.Sc.
+            <span className={styles.subtitle}>Freelance developer from the Netherlands</span>
+          </h2>
+        </div>
+
+        <h1>
+          With over <strong>eight years of Frontend Development experience</strong>, I specialize in crafting scalable
+          solutions using React and TypeScript.
+        </h1>
+      </section>
+    </Container>
+  );
+};
