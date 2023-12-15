349490f2872b65f9eae93b3637f0e35b408a21ad
Lencodes <contact@lencodes.com>
2023-12-15 19:18:18 +0100

    ADD tilting on hover to project cards

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./ProjectCard.module.scss";
+import { Tilt } from "react-tilt";
 
 export type TProjectCard = {
   title: string;
@@ -19,14 +20,16 @@ export const ProjectCard: React.FC<ProjectCardProps & TProjectCard> = ({
   backgroundImageSrc,
 }) => {
   return (
-    <div className={styles.container} style={{ backgroundImage: `url(${backgroundImageSrc})` }}>
-      <div className={styles.content}>
-        <img className={styles.logo} src={logoSrc} alt={title} />
+    <Tilt className={styles.tilt} options={{ max: 10, perspective: 1000, scale: 1 }}>
+      <div className={styles.container} style={{ backgroundImage: `url(${backgroundImageSrc})` }}>
+        <div className={styles.content}>
+          <img className={styles.logo} src={logoSrc} alt={title} />
 
-        <h3>
-          {title} <span className={styles.subtitle}>{subtitle}</span>
-        </h3>
+          <h3>
+            {title} <span className={styles.subtitle}>{subtitle}</span>
+          </h3>
+        </div>
       </div>
-    </div>
+    </Tilt>
   );
 };
