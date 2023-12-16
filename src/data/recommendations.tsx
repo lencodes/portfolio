81f9a1fc92fc53f0343f541a3c12d3c40c143ce5
Lencodes <contact@lencodes.com>
2023-12-16 22:08:49 +0100

    ADD recommendations data

---
+++ src/data/recommendations.tsx
@@ -0,0 +1,37 @@
+import { TRecommendation } from "../components/recommendationCard/RecommendationCard";
+
+export const recommendations: TRecommendation[] = [
+  {
+    id: "0d134cf6-64ab-4516-b489-9a800d70f9ce",
+    name: "Marleen Romijn",
+    role: "Co-owner at Conduction",
+    profilePhotoSrc: "/images/recommendations/review-marleen-romijn.jpg",
+    review:
+      "Lennart is a versatile, honest and expert Senior developer. He has very strong communication skills, both internally and externally, he knows how to peel the work/assignment apart very well and explain it clearly. Guiding others comes naturally to him very well. He also has a good view of the entire playing field of a company and likes to think along, which makes him different from others and is of enormous added value. Furthermore, Lennart is very easy and pleasant to deal with, which is also not unimportant. In short, I wish every company a Lennart!",
+  },
+  {
+    id: "b4a61897-14c1-42fe-b697-2a51c62e0848",
+    name: "Nienke van Heusden",
+    role: "Managing Director at Kaliber",
+    profilePhotoSrc: "/images/recommendations/review-nienke-van-heusden.jpg",
+    review:
+      "Lennart worked at Kaliber Interactive for a number of months on various projects. The nice thing about our collaboration is that Lennart has very strong communication skills, both internally and externally. He ensures that he has all the information to get a good start on a project. He also provides a clear update of the status and progress during a project, including daily's. With Lennart you gain a lot of knowledge and professionalism. Our collaboration has been very successful!",
+  },
+  {
+    id: "fcd9b7e4-eddd-449a-bfbb-50f62b6cdc2d",
+    name: "Bas van der Pol",
+    role: "CTO & Co-Founder at Ciphix",
+    profilePhotoSrc: "/images/recommendations/review-bas-van-der-pol.jpg",
+    review:
+      "Lennart is pragmatic and result driven. Fun to work with, easy to connect with both personally and professionally. During his work at Ciphix Lennart developed both technical and leadership skills. Lennart is strong technically, but also able to present ideas to internal team members and clients. I’d say any company should feel lucky having Lennart on-board.",
+    originalLanguage: "en",
+  },
+  {
+    id: "ad86fc91-d962-443d-ba88-99fc6684497a",
+    name: "Wilco Louwerse",
+    role: "Developer at Conduction",
+    profilePhotoSrc: "/images/recommendations/review-wilco-louwerse.jpg",
+    review:
+      "I worked with Lennart for two years at Conduction. In his role as a senior front-end developer, Lennart has taken our React codebases to the next level with his in-depth knowledge and experience by adding more structure. Lennart is not only an expert in his field, but also a colleague with whom it is a pleasure to work. His clear communication and positive contribution have significantly strengthened the team. I am confident that Lennart will be a valuable asset wherever he goes.",
+  },
+];
