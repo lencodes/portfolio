41ecd9a791c0c21bed04f40313f2c5cc8ac52a07
Lencodes <contact@lencodes.com>
2023-12-19 11:48:26 +0100

    REFACTOR skills data

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,18 +1,34 @@
 import { TSkill } from "../components/skill/Skill";
 
 export const skills: Array<TSkill> = [
-  { label: "React", mastered: true },
-  { label: "TypeScript", mastered: true },
-  { label: "JavaScript", mastered: true },
-  { label: "WordPress", mastered: true },
-  { label: "PHP", mastered: true },
-  { label: "Agile", mastered: true },
-  { label: "Scrum", mastered: true },
-  { label: "UI Design" },
-  { label: "UX Design" },
-  { label: "Sketch" },
-  { label: "HTML5", mastered: true },
-  { label: "CSS3 / SCSS", mastered: true },
-  { label: "Component Libraries", mastered: true },
-  { label: "REST", mastered: true },
+  { iconSrc: "/images/skills/typescript.svg", label: "TypeScript", highlighted: true },
+  { iconSrc: "/images/skills/javascript.svg", label: "JavaScript", highlighted: true },
+  { iconSrc: "/images/skills/php.svg", label: "PHP", highlighted: true },
+  { iconSrc: "/images/skills/css.svg", label: "CSS", highlighted: true },
+  { iconSrc: "/images/skills/html.svg", label: "HTML", highlighted: true },
+
+  { iconSrc: "/images/skills/react.svg", label: "React", highlighted: true },
+  { iconSrc: "/images/skills/gatsby.svg", label: "GatsbyJS", highlighted: true },
+  { iconSrc: "/images/skills/wordpress.svg", label: "WordPress", highlighted: true },
+  { iconSrc: "/images/skills/vue.svg", label: "Vue" },
+
+  { iconSrc: "/images/skills/git.svg", label: "Git", highlighted: true },
+  { iconSrc: "/images/skills/github.svg", label: "GitHub Workflows", highlighted: true },
+  { iconSrc: "/images/skills/jira.svg", label: "JIRA", highlighted: true },
+  { iconSrc: "/images/skills/teams.svg", label: "Teams" },
+  { iconSrc: "/images/skills/slack.svg", label: "Slack", highlighted: true },
+
+  { iconSrc: "/images/skills/json.svg", label: "JSON", highlighted: true },
+  { iconSrc: "/images/skills/xml.svg", label: "XML" },
+  { iconSrc: "/images/skills/rest.svg", label: "REST", highlighted: true },
+
+  { iconSrc: "/images/skills/sketch.svg", label: "Sketch", highlighted: true },
+  { iconSrc: "/images/skills/figma.svg", label: "Figma" },
+  { iconSrc: "/images/skills/photoshop.svg", label: "Photoshop" },
+
+  { iconSrc: "/images/skills/docker.svg", label: "Docker" },
+  { iconSrc: "/images/skills/nodejs.svg", label: "NodeJS" },
+  { iconSrc: "/images/skills/elasticsearch.svg", label: "ElasticSearch" },
+  { iconSrc: "/images/skills/mysql.svg", label: "MySQL" },
+  { iconSrc: "/images/skills/mongodb.svg", label: "MongoDB" },
 ];
