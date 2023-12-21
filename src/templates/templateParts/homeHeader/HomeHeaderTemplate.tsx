93864195c4cf210369759fe7fd7e680b9b7c90d5
Lencodes <contact@lencodes.com>
2023-12-21 16:29:39 +0100

    ADD home header animation

--- src/templates/templateParts/homeHeader/HomeHeaderTemplate.tsx
+++ src/templates/templateParts/homeHeader/HomeHeaderTemplate.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import styles from "./HomeHeaderTemplate.module.scss";
 import { SkillsList } from "../../../components/skillsList/SkillsList";
+import { animated, config, useSpring } from "react-spring";
 
 export const HomeHeaderTemplate: React.FC = () => {
+  const bounceInHeadshot = useSpring({
+    from: { opacity: 0, transform: "translateY(50px)" },
+    to: {
+      opacity: 1,
+      transform: "translateY(20px)",
+    },
+    config: config.wobbly,
+  });
+
   return (
     <section className={styles.container}>
-      <div className={styles.headshot}>
+      <animated.div style={bounceInHeadshot} className={styles.headshot}>
         <img src="/images/global/lennart-bank-profile-picture.jpg" alt="Lennart Bank" />
 
         <h2>
           Lennart Bank, B.Sc.
           <span className={styles.subtitle}>Freelance developer from the Netherlands</span>
         </h2>
-      </div>
+      </animated.div>
 
       <h1>
         With over <strong>eight years of Frontend Development experience</strong>, I specialize in crafting scalable
